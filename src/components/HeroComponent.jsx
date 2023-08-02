ab4d33b40e84c4fbb60d92ae9c95a2067ce2cd92
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-02 13:35:05 +0530

    made mobile responsive

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -13,33 +13,28 @@ const handleScroll = () => {
 
 const HeroComponent = () => {
   return (
-    <section className="w-11/12 m-auto h-[90vh]">
-      <div className="mt-10 flex flex-col lg:flex-row relative ">
-        <div className="mt-10 tracking-wider z-20 text-slate-800 w-11/12 m-auto">
-          <h1 className="mt-[60px] lg:mt-[150px] text-4xl lg:text-7xl font-extrabold ">
+    <section className="xl:w-4/5 m-auto h-[90vh]">
+      <div className="xl:mx-10 xl:mt-5 flex flex-col justify-between lg:flex-row  ">
+        <div className="mt-5 xl:mt-10 tracking-wider z-20 text-slate-800 w-11/12 m-auto">
+          <h1 className="mt-[60px] text-4xl lg:mt-[150px] xl:text-7xl font-extrabold ">
             Welcome to,{" "}
-            <span className="text-red-600 uppercase">store-X11</span>
+            <span className="text-red-600 text-6xl xl:text-7xl uppercase">
+              store-X11
+            </span>
           </h1>
-          <h2 className="mt-2 lg:mt-8 text-3xl lg:text-6xl font-bold capitalize">
+          <h2 className="mt-2 lg:mt-8 text-3xl lg:text-5xl font-bold capitalize">
             the only store you'll ever need...
           </h2>
           <button
             onClick={handleScroll}
-            className="mt-10 ml-2 px-12 py-4 rounded-full text-xl text-white font-semibold bg-red-600 hover:bg-gray-200 hover:text-black"
+            className="mt-5 mb-10 px-5 py-2 xl:mt-10 xl:mb-0 xl:ml-2 xl:px-12 xl:py-4 rounded-full text-xl text-white font-semibold bg-red-600 hover:bg-gray-200 hover:text-black"
           >
             Browse Products
           </button>
         </div>
-        <div className="absolute top-60 left-[12em] lg:top-20 lg:left-[64em] lg:right-10 z-10">
+        <div className="xl:mt-20 w-11/12 xl:w-[70%]  m-auto">
           <img src={HeroBG} alt="heroimage" />
         </div>
-        {/* <div className="absolute lg:-top-36">
-          <img
-            className="ml-24 h-[600px] opacity-90 lg:h-full lg:ml-[22em] lg:w-5/6 "
-            src={Background}
-            alt="bgimage"
-          />
-        </div> */}
       </div>
     </section>
   );
