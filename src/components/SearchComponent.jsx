a74b8e001371f8f38f1f01c6a4472354dba770d5
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-04 19:15:15 +0530

    commit

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useState, Fragment } from "react";
 import { ApiContext } from "../context/ApiContext";
 import { Combobox, Transition } from "@headlessui/react";
 import { CheckIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
+import SearchResult from "./SearchResult";
 
 const product = [
   "phone",
@@ -15,8 +17,9 @@ const product = [
 ];
 
 const SearchComponent = () => {
-  const { searchProducts } = useContext(ApiContext);
-  const [selectedPerson, setSelectedPerson] = useState(product);
+  const { searchProducts, searchCategory, setsearchProducts } =
+    useContext(ApiContext);
+  const [selectedProduct, setSelectedProduct] = useState(product);
   const [query, setQuery] = useState("");
 
   const filteredProduct =
@@ -32,7 +35,7 @@ const SearchComponent = () => {
 
   return (
     <section className="lg:mt-20 lg:ml-12">
-      <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+      <Combobox value={searchProducts} onChange={setsearchProducts}>
         <div className="relative mt-6 w-full">
           <Combobox.Input
             className=" w-4/6 text-2xl bg-white rounded-tl-lg rounded-bl-lg px-5 py-3"
@@ -40,22 +43,27 @@ const SearchComponent = () => {
             displayValue={query}
             onChange={handleChange}
           />
-          <button className="px-5 py-3 bg-red-600 text-white font-semibold hover:bg-gray-800 hover:text-white text-2xl rounded-tr-lg rounded-br-lg ">
+          <Link
+            onClick={() => searchCategory()}
+            to={`/search/${searchProducts}`}
+            className="px-5 py-3 bg-red-600 text-white font-semibold hover:bg-gray-800 hover:text-white text-2xl rounded-tr-lg rounded-br-lg "
+          >
             Search
-          </button>
+          </Link>
           <Transition
             as={Fragment}
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            afterLeave={() => setQuery("")}
+            afterLeave={() => setQuery(query)}
           >
             <Combobox.Options className="mt-2">
               {filteredProduct.map((data) => (
                 <Combobox.Option
+                  onChange={() => setsearchProducts(data)}
                   key={data}
                   className={({ active }) =>
-                    `relative text-lg px-5 py-2 w-4/6 capitalize bg-white ${
+                    `relative text-lg px-5 py-2 w-4/6 capitalize bg-gray-100 ${
                       active
                         ? "bg-red-500 cursor-pointer text-white"
                         : "text-gray-900"
@@ -70,7 +78,6 @@ const SearchComponent = () => {
           </Transition>
         </div>
       </Combobox>
-
       {/* <form className="relative lg:w-2/4  bg-gray-100 m-4 py-2 px-4 border-2 border-gray-400 rounded-lg">
         <input
           className="focus:outline-0 text-lg lg:w-11/12 h-8 bg-gray-100 "
@@ -93,6 +100,9 @@ const SearchComponent = () => {
           />
         </svg>
       </form> */}
+      <div>
+        <SearchResult searchProduct={searchProducts} />
+      </div>
     </section>
   );
 };
