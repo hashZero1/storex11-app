7a3ae70e7444d9cf4c52acf8c311cd84bb9cd264
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-05 17:48:05 +0530

    configured user auth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, createContext, useEffect } from "react";
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "../Firebase/Firebase.config";
+//Context to track the user sign in or sign out
 
-const AuthContext = () => {
-  return <div>AuthContext</div>;
+// actual value/fn you want to access
+// current user check the existing user obj or null (bcoz empty obj evaluate as true)
+// and empty function(setCurrentUser) which return null
+export const AuthContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => null,
+});
+//user provider is the component
+//this function allow us to access the user value inside the useState
+//which we can call and get the value any component
+export const AuthProvider = ({ children }) => {
+  //to store user state with initial value is null
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
+
+  //to monitor the user signed in or out
+  useEffect(() => {
+    const unsubscibe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
+    return unsubscibe;
+  }, []);
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
-
-export default AuthContext;
