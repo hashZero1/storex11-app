ecf0d402d78cb64acf33c0f453679a550f4c0502
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-12 15:18:42 +0530

    add category result

--- src/components/CatergoryComponent.jsx
+++ src/components/CatergoryComponent.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { ApiContext } from "../context/ApiContext";
-import { CartContext } from "../context/CartContext";
+
 import { motion } from "framer-motion";
+import { CategoryResult } from "./CategoryResult";
 
 const categories = [
   "mens-watches",
@@ -23,17 +24,15 @@ const categories = [
 
 const CatergoryComponent = () => {
   const { category, selectItem, setSelectItem } = useContext(ApiContext);
-  const { addItemToCart, notify } = useContext(CartContext);
 
   const handleItemClick = (item) => {
     setSelectItem(item);
   };
 
-  console.log(selectItem);
   return (
     <div className="w-11/12 mx-auto mt-20 ">
       <h1 className="text-3xl ml-14 mb-5 font-semibold">Product categories</h1>
-      <div className=" pb-4 mx-auto flex overflow-x-scroll yes-scrollbar">
+      <div className="pb-4 mx-auto lg:w-11/12 flex overflow-x-scroll yes-scrollbar">
         {categories.map((dt) => (
           <motion.button
             whileHover={{ scale: 1.1 }}
@@ -41,7 +40,7 @@ const CatergoryComponent = () => {
             className={` ${
               selectItem ? "active:bg-red-400 focus:bg-red-600" : "bg-red-200 "
             }
-           bg-red-100 bg-opacity-50 capitalize rounded-lg m-2 px-5 py-4 text-2xl hover:bg-red-500 whitespace-nowrap transition-all shadow-md `}
+           bg-red-100 bg-opacity-50 capitalize rounded-lg m-1  px-4 py-2 text-lg lg:m-2 lg:px-5 lg:py-4 lg:text-2xl hover:bg-red-500 whitespace-nowrap transition-all shadow-md `}
             key={dt.id}
             onClick={() => {
               handleItemClick(dt);
@@ -51,54 +50,12 @@ const CatergoryComponent = () => {
           </motion.button>
         ))}
       </div>
-
-      <div className="">
+      <div>
         {category ? (
-          <>
-            <div className="flex mt-10 flex-wrap justify-center">
-              {category?.map((dt) => (
-                <motion.div
-                  whileHover={{ scale: 1.02 }}
-                  key={dt.id}
-                  className="p-2"
-                >
-                  <div className="max-w-sm  m-2 bg-opacity-50 bg-white rounded-lg shadow ">
-                    <div className="p-4 rounded-t-lg bg-white">
-                      <img
-                        className=" w-full h-48 object-contain"
-                        src={dt.images[0]}
-                        alt=""
-                      />
-                    </div>
-
-                    <div className="h-60 p-5">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-700">
-                        {dt.title}
-                      </h5>
-
-                      <p className="mb-3 h-[105px] font-normal text-gray-700 dark:text-gray-600">
-                        {dt.description} the biggest enterprise technology
-                        acquisitions of 2021 so far.
-                      </p>
-
-                      <button
-                        onClick={() => {
-                          addItemToCart(dt);
-                          notify();
-                        }}
-                        className="px-4 py-2 bg-red-600 text-white font-semibold hover:bg-gray-800 hover:text-white rounded-lg"
-                      >
-                        Add To Cart
-                      </button>
-                    </div>
-                  </div>
-                </motion.div>
-              ))}
-            </div>
-          </>
+          <CategoryResult category={category} />
         ) : (
-          <h1 className="text-3xl m-4">
-            ...Sorry Product is not available(Double click any category)
+          <h1 className="text-3xl text-center mx-auto bg-red-200 py-4 px-10 m-4">
+            ...Sorry Product is not available at the moment visit later
           </h1>
         )}
       </div>
