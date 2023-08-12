f69361f009ee68abb6153169b5cbcb5f96af882e
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-12 15:09:25 +0530

    separated the results from category

---
+++ src/components/CategoryResult.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { motion } from "framer-motion";
+
+export const CategoryResult = ({ category }) => {
+  const { addItemToCart, notify } = useContext(CartContext);
+  return (
+    <div className="flex mt-10 flex-wrap justify-center">
+      {category?.map((dt) => (
+        <motion.div whileHover={{ scale: 1.02 }} key={dt.id} className="p-2">
+          <div className="max-w-sm m-2 lg:m-0 2xl:m-2 bg-opacity-50 bg-white rounded-lg shadow ">
+            <div className="p-4 rounded-t-lg bg-white">
+              <img
+                className=" w-full h-48 object-contain"
+                src={dt.images[0]}
+                alt=""
+              />
+            </div>
+
+            <div className="h-60 p-5">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-700">
+                {dt.title}
+              </h5>
+
+              <p className="mb-3 h-[105px] font-normal text-gray-700 dark:text-gray-600">
+                {dt.description} the biggest enterprise technology acquisitions
+                of 2021 so far.
+              </p>
+
+              <button
+                onClick={() => {
+                  addItemToCart(dt);
+                  notify();
+                }}
+                className="px-4 py-2 bg-red-600 text-white font-semibold hover:bg-gray-800 hover:text-white rounded-lg"
+              >
+                Add To Cart
+              </button>
+            </div>
+          </div>
+        </motion.div>
+      ))}
+    </div>
+  );
+};
