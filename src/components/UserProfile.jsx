0767048896034c9e2bd6b3fb81bb8ad7246eefe2
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-16 10:47:58 +0530

    add user profile

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { Avatar, Dropdown } from "flowbite-react";
+
+export default function UserProfile({ signOutHandler }) {
+  const { currentUser } = useContext(AuthContext);
+  return (
+    <>
+      <Dropdown
+        inline
+        label={
+          <Avatar alt="User settings" img={currentUser.photoURL} rounded />
+        }
+      >
+        <Dropdown.Header>
+          <span className="block text-sm font-semibold">
+            {currentUser.displayName}
+          </span>
+          <span className="block truncate text-sm font-medium">
+            {currentUser.email}
+          </span>
+        </Dropdown.Header>
+        <Dropdown.Divider />
+
+        <Link
+          className="block px-4 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+          onClick={signOutHandler}
+          to="/"
+        >
+          SignOut
+        </Link>
+      </Dropdown>
+    </>
+  );
+}
