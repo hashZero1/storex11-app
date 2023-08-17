55f52d9d7fce86c123dd9a985c661fab3eece384
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-17 14:53:56 +0530

    commit

--- src/components/CheckoutComponent.jsx
+++ src/components/CheckoutComponent.jsx
@@ -1,9 +1,23 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import {
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+} from "../Firebase/Firebase.config";
 
 export default function Checkout() {
   const { cartItems, cartTotal } = useContext(CartContext);
+  const { currentUser } = useContext(AuthContext);
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  const emptyCart = () => (cartItems.length = []);
+
   return (
     <div className="m-2 px-1 py-5 flex flex-col align-middle rounded-lg  shadow-md bg-red-200">
       <h1 className="mt-2 text-2xl text-center">Shopping Cart</h1>
@@ -13,12 +27,23 @@ export default function Checkout() {
           Subtotal ({cartItems.length} item) : ₹ {cartTotal}
         </h2>
       </div>
-      <Link
-        to="/auth"
-        className="text-center px-4 py-2 w-40 m-auto text-lg text-white  rounded-lg bg-red-600"
-      >
-        Proceed To Buy
-      </Link>
+      {currentUser ? (
+        <Link
+          to="/order"
+          onClick={emptyCart}
+          className="text-center py-2 w-40 m-auto text-lg text-white rounded-lg bg-red-600"
+        >
+          Place Order Now
+        </Link>
+      ) : (
+        <Link
+          onClick={signInWithGoogle}
+          className="text-center px-4 py-2 w-40 m-auto text-lg text-white  rounded-lg bg-red-600"
+          to="/"
+        >
+          Proceed To Buy
+        </Link>
+      )}
     </div>
   );
 }
