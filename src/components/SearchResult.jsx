781c37cca3f059dc418843ee91e195a20767fbcb
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-21 13:46:57 +0530

    commit

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,11 +1,12 @@
-import React, { memo, useContext } from "react";
+import React, { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { useParams, useSearchParams } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const SearchResult = ({ searchProduct }) => {
   const { id } = useSearchParams();
   // const searchDetails = searchProduct.find((pd) => pd.id === parseInt(id));
-  const { addItemToCart } = useContext(CartContext);
+  const { addItemToCart, notify } = useContext(CartContext);
   const currentPath = window.location.pathname === `/search/:${id}`;
 
   return (
@@ -18,7 +19,11 @@ const SearchResult = ({ searchProduct }) => {
 
           <div className="flex mt-5 flex-wrap justify-center">
             {searchProduct.products?.map((dt) => (
-              <div key={dt.id} className="p-2">
+              <motion.div
+                whileHover={{ scale: 1.02 }}
+                key={dt.id}
+                className="p-2"
+              >
                 <div className="max-w-sm m-2 lg:m-0 2xl:m-2 bg-opacity-50 bg-white rounded-lg shadow ">
                   <div className="p-4 rounded-t-lg bg-white">
                     <img
@@ -28,7 +33,7 @@ const SearchResult = ({ searchProduct }) => {
                     />
                   </div>
 
-                  <div className="h-60 p-5">
+                  <div className="min-h-min p-5">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-700">
                       {dt.title}
                     </h5>
@@ -39,14 +44,17 @@ const SearchResult = ({ searchProduct }) => {
                     </p>
 
                     <button
-                      onClick={() => addItemToCart(dt)}
-                      className="px-4 py-2 bg-red-600 text-white font-semibold hover:bg-gray-800 hover:text-white transition-all rounded-lg"
+                      onClick={() => {
+                        addItemToCart(dt);
+                        notify();
+                      }}
+                      className="px-4 py-2 bg-red-600 text-white font-semibold hover:bg-gray-800 hover:text-white rounded-lg"
                     >
                       Add To Cart
                     </button>
                   </div>
                 </div>
-              </div>
+              </motion.div>
             ))}
           </div>
         </section>
