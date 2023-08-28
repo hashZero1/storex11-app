e1e6e7e83b864626fcbf9420c3a658f77923fd11
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-28 12:24:39 +0530

    added confirmation modal

--- src/components/ConfirmationPopup.jsx
+++ src/components/ConfirmationPopup.jsx
@@ -1,25 +1,55 @@
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
+import { Button, Modal } from "flowbite-react";
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
 
 export default function ConfirmationPopup() {
-  let [isOpen, setIsOpen] = useState(true);
+  const { cartItems } = useContext(CartContext);
+  const [openModal, setOpenModal] = useState();
+  const props = { openModal, setOpenModal };
 
+  const emptyCart = () => (cartItems.length = []);
   return (
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-      <Dialog.Panel>
-        <Dialog.Title>Deactivate account</Dialog.Title>
-        <Dialog.Description>
-          This will permanently deactivate your account
-        </Dialog.Description>
-
-        <p>
-          Are you sure you want to deactivate your account? All of your data
-          will be permanently removed. This action cannot be undone.
-        </p>
-
-        <button onClick={() => setIsOpen(false)}>Deactivate</button>
-        <button onClick={() => setIsOpen(false)}>Cancel</button>
-      </Dialog.Panel>
-    </Dialog>
+    <>
+      <Button
+        className="text-center py-2 w-40 m-auto text-lg text-white rounded-lg bg-red-600"
+        onClick={() => props.setOpenModal("pop-up")}
+      >
+        Place Order Now
+      </Button>
+      <Modal
+        show={props.openModal === "pop-up"}
+        size="md"
+        popup
+        onClose={() => props.setOpenModal(undefined)}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Order Confirmation ?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Link
+                to="/order"
+                className="text-center py-2 w-40 text-white rounded-lg bg-red-600"
+                onClick={() => {
+                  props.setOpenModal(undefined);
+                  emptyCart();
+                }}
+              >
+                Yes, I'm sure
+              </Link>
+              <Button
+                color="gray"
+                onClick={() => props.setOpenModal(undefined)}
+              >
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
