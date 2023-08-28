677f7eee36496b0366339d1cb7d49219ac765df8
Animesh Sharma <117970097+hashZero1@users.noreply.github.com>
2023-08-28 12:22:07 +0530

    add confirmation dialog

--- src/components/CheckoutComponent.jsx
+++ src/components/CheckoutComponent.jsx
@@ -6,6 +6,7 @@ import {
   createUserDocumentFromAuth,
   signInWithGooglePopup,
 } from "../Firebase/Firebase.config";
+import ConfirmationPopup from "./ConfirmationPopup";
 
 export default function Checkout() {
   const { cartItems, cartTotal } = useContext(CartContext);
@@ -16,25 +17,18 @@ export default function Checkout() {
     await createUserDocumentFromAuth(user);
   };
 
-  const emptyCart = () => (cartItems.length = []);
-
   return (
     <div className="m-2 px-1 py-5 flex flex-col align-middle rounded-lg  shadow-md bg-red-200">
       <h1 className="mt-2 text-2xl text-center">Shopping Cart</h1>
-
       <div className="p-2 mx-2 my-3 text-xl text-center rounded-lg font-semibold xl:text-2xl bg-gray-800 text-white">
         <h2>
           Subtotal ({cartItems.length} item) : ₹ {cartTotal}
         </h2>
       </div>
       {currentUser ? (
-        <Link
-          to="/order"
-          onClick={emptyCart}
-          className="text-center py-2 w-40 m-auto text-lg text-white rounded-lg bg-red-600"
-        >
-          Place Order Now
-        </Link>
+        <>
+          <ConfirmationPopup />
+        </>
       ) : (
         <Link
           onClick={signInWithGoogle}
